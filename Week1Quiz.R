0b2f55adc19e1ea439e241895855c959c9c87156
Santiago Fernandez <cenybar@gmail.com>
2019-04-17 14:04:05 +0200

    Add Week 1 Quiz Answers

---
+++ Week1Quiz.R
@@ -0,0 +1,93 @@
+1 # Suppose I conduct a study and publish my findings. Which of the following is an example of a replication of my 
+# study?
+
+# I give my data to an independent investigator at another institution, she analyzes the data and gets the same 
+#results as I originally obtained. CORRECT ANSWER
+
+#An investigator at another institution conducts a study addressing a different scientific question and publishes
+# her findings.
+
+#An investigator at another institution conducts a study addressing the same question, collects her own data, 
+#analyzes it separately from me, and publishes her own findings.
+
+#I take my own data, analyze it again, and publish new findings.
+
+2 # Which of the following is a requirement for a published data analysis to be reproducible?
+
+# The analysis is conducted on a variant of the Unix operating system.
+# The investigator's final publication is made available free of charge.
+# The full computer code for doing the data analysis is made publicly available. CORRECT ANSWER
+# The data analysis is conducted using R.
+
+3 #Which of the following is an example of a reproducible study?
+
+#The study's analytic data and computer code are not publicly available, but the study was simple enough to be 
+#repeated by an independent investigator.
+
+#The study's analytic data are publicly available, but the computer code is not.
+
+# The study's original authors re-run their computer code on their analytic data and confirm publicly that the 
+# findings match those of the published results.
+
+# The study's analytic data and computer code for the data analysis are publicly available. When the code is run 
+# on the analytic data, the findings are identical to the published results. CORRECT ANSWER
+
+4 # Which of the following is a reason that a study might NOT be fully replicated?
+
+# The original study was opportunistic in its timing and it would be difficult to find a similar context in which
+# to repeat it. CORRECT ANSWER
+
+# The original study had null findings.
+
+# The original study was conducted by a well-known investigator.
+
+# The original study was published in a high impact journal and is considered authoritative.
+
+5 # Which of the following is a reason why publishing reproducible research is increasingly important?
+        
+# New technologies are increasing the rate of data collection, creating datasets that are more complex and 
+# extremely high dimensional. CORRECT ANSWER
+
+# Most studies today are small-scale and easily replicated.
+
+# Computing power is limited today, making it difficult to apply sophisticated statistical methods.
+
+# The statistical methods for most studies can be accurately described using plain language.        
+        
+
+6 # What is the role of processing code in the research pipeline?
+
+# It transforms the measured data into analytic data. CORRECT ANSWER
+# It transforms the computational results into figures and tables.
+# It conducts the statistical analysis of the primary outcome.
+# It transforms the analytic data into computational results.
+
+7 # Which is a goal of literate statistical programming?
+        
+# Separate figures and tables from other data analytic summaries.
+# Require that data analysis summaries are always written in LaTeX.
+# Combine explanatory text and data analysis code in a single document. CORRECT ANSWER
+# Ensure that data analysis documents are always exported in PDF format.
+        
+8 # What does it mean to weave a literate statistical program?
+    
+# Transform a literate program from R to python.
+# Transform the literate program into a machine readable code file.
+# Transform the literate program into a human readable document. CORRECT ANSWER
+# Compress the literate program so that it takes up less space.
+
+9 # Which of the following is required to implement a literate programming system?
+        
+# A program that views PDF files.
+# A web server for publishing documents.
+# A documentation language like LaTeX. CORRECT ANSWER
+# A Unix-based computer system.
+
+10 # What is one way in which the knitr system differs from Sweave?
+        
+# knitr lacks features like caching of code chunks.
+# knitr is written in python instead of R.
+# knitr was developed by Friedrich Leisch.
+# knitr allows for the use of markdown instead of LaTeX. CORRECT ANSWER
+
+
